2679b9831602df91c643ce69fab65585e3396f08
Ho Dung Son <dungson0902@gmail.com>
2020-08-04 17:48:40 +0700

    Update design

---
+++ WF.Web/Models/SchemeDeleteResultModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WF.Web.Models
+{
+    public class SchemeDeleteResultModel
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+    }
+}
